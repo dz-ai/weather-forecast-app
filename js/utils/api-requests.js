70e2ecfe79ddbaa32c0a11858d7a08d5e0d32017
dz-ai <82286961+dz-ai@users.noreply.github.com>
2022-10-11 23:12:11 +0300

    Update api-requests.js

--- js/utils/api-requests.js
+++ js/utils/api-requests.js
@@ -3,11 +3,11 @@ const urlCity = 'https://api.openweathermap.org/data/2.5/weather?';
 const urlForecast = 'https://api.openweathermap.org/data/2.5/onecall?';
 
 export async function citySearch (cityName) {
-   const response = await fetch(urlCity + `q=${cityName}&appid=${secrets.APIKEY}`);
+   const response = await fetch(urlCity + `q=${cityName}&appid=${APIKEY}`);
     return await response.json()
 }
 
 export async function foreCastSearch (lat, lon, unit) {
-  const response = await fetch(urlForecast + `lat=${lat}&lon=${lon}&units=${unit}&appid=${secrets.APIKEY}`);
+  const response = await fetch(urlForecast + `lat=${lat}&lon=${lon}&units=${unit}&appid=${APIKEY}`);
   return response.json()
 }
